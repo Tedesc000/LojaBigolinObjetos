714c5a9e6f12cae40aad27f8f12efe9f8733bf15
EduardoTedesco <eduardo.tedesco19@gmail.com>
2026-09-23 14:46:35 -0300

    fazendo o usuario.js com classes em javascript

--- AtividadeLoja/usuario.js
+++ AtividadeLoja/usuario.js
@@ -1,17 +1,41 @@
-function usuario(nome, dataNasc, senha){
-    this.nome = nome;
-    let dataNasc = dataNasc;
-    let idade;
+function usuario(nome, dataNasc, email, senha){
+    this.nome = nome;//publico
+    let dataNasc = validaDataNasc(dataNasc);
+    let idade;//privada
+    let email = validaEmail(email);
 
-    this.verificaSenha = function(senha){
-        const maiuscula = senha.toUpperCase();
-        const minuscula = senha.toLowerCase();
-        
+    this.setSenha(senha){
+        this.senha = validaSenha(senha);
+    }
+    this.getSenha(){
+        return this.senha;
     }
 
+
+    this.getEmail = function(){
+        return email;
+    }
+
+    this.validaEmail = function(){
+
+    }
+
+    this.validaSenha = function(senha){
+        let maiuscula = senha.toUpperCase();
+        let minuscula = senha.toLowerCase();
+        
+        if(senha !== maiuscula && senha !== minuscula){
+            return this.senha = senha;
+        }else{
+            throw new Exception("A senha é muito fraca");
+        }
+    }
+
+
+
     this.calculaIdade = function(dataNasc){
-        const diaHoje = new Date();
-        const nascimento = new Date(dataNasc);
+        let diaHoje = new Date();
+        let nascimento = new Date(dataNasc);
         idade = diaHoje.getFullYear() - nascimento.getFullYear;
     }
 }
